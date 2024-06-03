fe65519368bb31bc9f6fec54ac42db9cf070e579
Ilya Barashkov <rete234@mail.ru>
2024-06-03 12:55:19 +0300

    done functions for users

--- ibibula/users/forms.py
+++ ibibula/users/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
-
 from users.models import User
+from main.models import Categories, Subcategories
 
 
 class UserLoginForm(AuthenticationForm):
-
     class Meta:
         model = User
         fields = ['username', 'password']
@@ -13,8 +12,8 @@ class UserLoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.CharField()
 
-class UserRegistrationForm(UserCreationForm):
 
+class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = (
@@ -25,7 +24,7 @@ class UserRegistrationForm(UserCreationForm):
             "phone_number",
             "telegram",
         )
-    
+
     username = forms.CharField()
     email = forms.CharField()
     password1 = forms.CharField()
@@ -42,14 +41,30 @@ class ProfileForm(UserChangeForm):
             "email",
             "phone_number",
             "telegram",
-            "password1",
-            "password2"
             "image",
         )
+
     username = forms.CharField()
     email = forms.CharField()
     phone_number = forms.CharField()
     telegram = forms.CharField()
-    password1 = forms.CharField()
-    password2 = forms.CharField()
     image = forms.ImageField(required=False)
+
+
+
+class ProductForm(forms.Form):
+    productname = forms.CharField()
+    price = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
+    image = forms.ImageField()
+    category = forms.ChoiceField(choices=[(category.id, category.name) for category in Categories.objects.all()])
+    subcategory = forms.ChoiceField(choices=[(subcategory.id, subcategory.name) for subcategory in Subcategories.objects.all()])
+
+    def clean_category(self):
+        category_id = self.cleaned_data['category']
+        return Categories.objects.get(id=category_id)
+
+    def clean_subcategory(self):
+        subcategory_id = self.cleaned_data['subcategory']
+        return Subcategories.objects.get(id=subcategory_id)
+
--- ibibula/users/views.py
+++ ibibula/users/views.py
@@ -1,96 +1,99 @@
-
 from django.contrib import auth, messages
 from django.shortcuts import redirect, render
 from django.template import context
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from django.http import HttpResponseRedirect
 
-from users.forms import  UserLoginForm, UserRegistrationForm, ProfileForm
-from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm, ProductForm
+from users.models import User, Performer
+from main.models import Services
+
 
 def login(request):
 
     users = User.objects.all()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = request.POST["username"]
+            password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
 
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:index'))
+                return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
 
-    context = {
-        'users':users,
-        'form':form
-    }
+    context = {"users": users, "form": form}
 
-    return render(request, 'users/login.html', context)
+    return render(request, "users/login.html", context)
+
 
 def profile(request):
 
-    context = {
-        'title':'Все для студентов'
-    }
+    context = {"title": "Все для студентов"}
 
-    return render(request, 'users/profile.html', context)
+    return render(request, "users/profile.html", context)
 
-def registration(request):
 
-    context = {
-        'title':'Все для студентов'
-    }
 
-    return render(request, 'users/registration.html', context)
-
+@login_required
 def edituser(request):
-    if request.method == 'POST':
-        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST, instance=request.user, files=request.FILES
+        )
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('user:profile'))
+            return HttpResponseRedirect(reverse("user:profile"))
     else:
         form = ProfileForm(instance=request.user)
-        
 
     context = {
-        'title': 'Home - Кабинет',
-        'form': form,
+        "title": "Home - Кабинет",
+        "form": form,
     }
-    return render(request, 'users/edituser.html', context)
-
+    return render(request, "users/editusers.html", context)
 
 def editservice(request):
+    if request.method == "POST":
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:profile"))
+    else:
+        form = ProductForm()
 
-    context = {
-        'title':'Все для студентов'
-    }
+    context = {"title": "Все для студентов", "form": form}
+    return render(request, "users/editservices.html", context)
 
-    return render(request, 'users/editservices.html', context)
+
 
 def registration(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('users:login'))
+            return HttpResponseRedirect(reverse("users:login"))
     else:
         form = UserRegistrationForm()
-    
-    context = {
-        'title': 'Все для студентов',
-        'form': form
-    }
-    return render(request, 'users/registration.html', context)
+
+    context = {"title": "Все для студентов", "form": form}
+    return render(request, "users/registration.html", context)
 
 
+
+
+
+
+
+
+@login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
-    return redirect(reverse('main:index'))
+    return redirect(reverse("main:index"))
